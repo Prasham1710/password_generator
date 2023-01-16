74efe925fd6a4e84fe2d4113bb3f342d87797dd2
Prasham1710 <jainprasham17@gmail.com>
2023-01-16 16:42:01 +0530

    top

--- src/App.js
+++ src/App.js
@@ -101,7 +101,7 @@ import { COPY_SUCCESS } from './Message'
           return (
           <div className="relative">
              <img className ='absolute w-full h-full object-cover mix-blend-overlay'src ='https://img.lovepik.com/photo/50055/7195.jpg_wh300.jpg'/>
-           <div className="flex relative justify-center pt-[100px]"><p  className="text-5xl bg-white">Generate  password</p> </div>
+           <div className="flex relative justify-center pt-[120px] pb-30"><p  className="text-5xl bg-white">Generate  password</p> </div>
         <div className="flex relative justify-center items-center h-full  pt-6 pb-16">
           < div className='generator'>
             <h2 className='generator__header'>Password Generator</h2>
