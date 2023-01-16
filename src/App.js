3f6ad7d1d945da48c4001f0ca6930dd5884b82d4
Prasham1710 <jainprasham17@gmail.com>
2023-01-16 16:49:58 +0530

    style

--- src/App.js
+++ src/App.js
@@ -103,7 +103,7 @@ import { COPY_SUCCESS } from './Message'
              <img className ='absolute w-full h-full object-cover mix-blend-overlay'src ='https://img.lovepik.com/photo/50055/7195.jpg_wh300.jpg'/>
            <div className="flex relative justify-center pt-[120px] pb-30"><p  className="text-5xl bg-white">Generate  password</p> </div>
         <div className="flex relative justify-center items-center h-full  pt-6 pb-16">
-          < div className='generator'>
+          < div className='bg-[#23235b] h-[450px] w-[400px] px-8 py-4 pt-6 pb-8 mb-4'>
             <h2 className='generator__header'>Password Generator</h2>
             <div className='generator__password'>
               <h3>{password}</h3><button onClick={handleCopyPassword} className='copy__btn'><BsClipboard/></button></div>
